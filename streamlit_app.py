5d682a5e13494c01f08347a02fcc9e8da436a913
liamnestimail-dotcom <liam.nesti.mail@gmail.com>
2026-08-29 16:36:23 +0000

    added image

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,17 +4,21 @@ st.title("welcome to🌱 my mini plant game 🌱a jump game with plants")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
-plants_list=["standert","spike","bomb","thunder","block"]
+plants_list=["standard","spike", "bomb","thunder"]
 #st.write(plants_list)
 #st.write("plant_list")
 #st.write(plants_list[0])
 #st.write(plants_list[1])
 #st.write(plants_list[2])
 #st.write(plants_list[3])
-#st.write(plants_list[4])
 #selected_plant=st.selectbox("skin",plants_list)
 #st.write("your plant is",selected_plant)
 selected_plant=None
 selected_plant=st.selectbox("your plant is",plants_list)
 if selected_plant:
     st.write("your plant is",selected_plant)
+
+images_list=["plant_images/standard.jpeg","plant_images/spike.jpeg","plant_images/bomb.jpeg","plant_images/thunder.jpeg"]
+st.image(images_list[0])
+
+
