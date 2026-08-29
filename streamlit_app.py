936364a94056bc06cb96eaeb61c74836f4fed7da
liamnestimail-dotcom <liam.nesti.mail@gmail.com>
2026-08-29 13:34:24 +0000

    changed title

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("welcome to🌱 my mini plant game 🌱a jump game with plants")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
