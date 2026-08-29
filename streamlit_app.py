bd447d897c305d7ac257513a340745c61a36fcd8
liamnestimail-dotcom <liam.nesti.mail@gmail.com>
2026-08-29 14:20:12 +0000

    added list

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,11 @@ st.title("welcome to🌱 my mini plant game 🌱a jump game with plants")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+plants_list=["standert","spike","bomb","thunder","block"]
+st.write(plants_list)
+st.write("plant_list")
+st.write(plants_list[0])
+st.write(plants_list[1])
+st.write(plants_list[2])
+st.write(plants_list[3])
+st.write(plants_list[4])
